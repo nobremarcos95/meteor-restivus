d3801d396d9233f308bbce508a1895d2b9d68628
Marcos Nobre <nobremarcos95@gmail.com>
2022-02-24 08:06:53 -0300

    be able to use on Meteor v2.3

--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'nimble:restivus',
   summary: 'Create authenticated REST APIs in Meteor 0.9+ via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
-  version: '0.8.12',
+  version: '0.8.13',
   git: 'https://github.com/kahmali/meteor-restivus.git'
 });
 
 
 Package.onUse(function (api) {
   // Minimum Meteor version
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom(['0.9.0', '2.3']);
 
   // Meteor dependencies
   api.use('check');
   api.use('coffeescript');
   api.use('underscore');
-  api.use('accounts-password@1.3.3');
-  api.use('simple:json-routes@2.1.0');
+  api.use('accounts-password');
+  api.use('simple:json-routes');
 
   api.addFiles('lib/auth.coffee', 'server');
   api.addFiles('lib/iron-router-error-to-response.js', 'server');
